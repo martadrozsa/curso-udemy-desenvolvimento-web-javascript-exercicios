97f4b971417e101eb613066d2dff5a8f0fc719f5
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-08-07 15:56:31 -0300

    Logic exercise - function to swap elements of two vectors

--- exercises/exercise39.js
+++ exercises/exercise39.js
@@ -2,3 +2,27 @@
 primeiro elemento do vetorA passe a ser o primeiro elemento do vetorB e vice versa e assim sucessivamente.
 Faça a troca sem utilizar uma variável auxiliar.
 */
+
+
+let aux = []
+function swap(vectorA, vectorB) {
+
+    if (vectorA.length == vectorB.length) {
+        for (let i = 0; i < vectorA.length; i++) {
+        aux[i] = vectorA[i]
+        vectorA[i] = vectorB[i]
+        vectorB[i] = aux[i]
+        } 
+    } else {
+        return 'The vectors have different sizes!'
+    }
+    
+    console.log(vectorA)
+    console.log(vectorB)
+}
+
+
+let numbersA = [10, 22, 33, 45]
+let numbersB = [21, 12, 30, 55]
+swap(numbersA, numbersB)
+
